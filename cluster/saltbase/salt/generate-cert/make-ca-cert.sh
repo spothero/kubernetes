0e97899cce313a192ad2daf46f116b10fdedb38f
Benjamin Goldberg <ben@spothero.com>
2017-04-26 15:01:50 -0500

    Also create kubeapiserver.cert/.key

--- cluster/saltbase/salt/generate-cert/make-ca-cert.sh
+++ cluster/saltbase/salt/generate-cert/make-ca-cert.sh
@@ -95,9 +95,15 @@ if [ $use_cn = "true" ]; then
     cp -p pki/issued/$cert_ip.crt "${cert_dir}/server.cert" > /dev/null 2>&1
     cp -p pki/private/$cert_ip.key "${cert_dir}/server.key" > /dev/null 2>&1
 else
+    # Generate server cert
     ./easyrsa --subject-alt-name="${sans}" build-server-full kubernetes-master nopass > /dev/null 2>&1
     cp -p pki/issued/kubernetes-master.crt "${cert_dir}/server.cert" > /dev/null 2>&1
     cp -p pki/private/kubernetes-master.key "${cert_dir}/server.key" > /dev/null 2>&1
+
+    # Generate kubeapiserver cert
+    ./easyrsa --subject-alt-name="${sans}" build-server-full kubeapiserver nopass > /dev/null 2>&1
+    cp -p pki/issued/kubeapiserver.crt "${cert_dir}/kubeapiserver.cert" > /dev/null 2>&1
+    cp -p pki/private/kubeapiserver.key "${cert_dir}/kubeapiserver.key" > /dev/null 3>&1
 fi
 # Make a superuser client cert with subject "O=system:masters, CN=kubecfg"
 ./easyrsa --dn-mode=org \
@@ -107,6 +113,6 @@ fi
 cp -p pki/ca.crt "${cert_dir}/ca.crt"
 cp -p pki/issued/kubecfg.crt "${cert_dir}/kubecfg.crt"
 cp -p pki/private/kubecfg.key "${cert_dir}/kubecfg.key"
-# Make server certs accessible to apiserver.
+#Make server certs accessible to apiserver.
 chgrp $cert_group "${cert_dir}/server.key" "${cert_dir}/server.cert" "${cert_dir}/ca.crt"
 chmod 660 "${cert_dir}/server.key" "${cert_dir}/server.cert" "${cert_dir}/ca.crt"
